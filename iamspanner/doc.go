8e59164a197d8ee31781110c56ecebfca1ef98fd
Oscar Söderlund <odsod@users.noreply.github.com>
2021-05-10 12:25:34 +0200

    docs: improve package comments
    Add missing comments and shorten existing ones for brevity.

--- iamspanner/doc.go
+++ iamspanner/doc.go
@@ -1,2 +1,2 @@
-// Package iamspanner provides a Spanner-based storage implementation of the iam.IAMPolicyService API.
+// Package iamspanner provides a Spanner-based storage implementation of the IAMPolicy service.
 package iamspanner
