bec2eb037608ef1c49ac8afec59ffc8ee7438261
Andreas Bäckevik <andreas.backevik@einride.tech>
2021-05-20 16:02:39 +0200

    chore: add apple silicon support for buf tooling

--- tools/buf/rules.mk
+++ tools/buf/rules.mk
@@ -3,7 +3,12 @@ buf_version := 0.41.0
 buf := $(buf_cwd)/$(buf_version)/bin/buf
 export PATH := $(dir $(buf)):$(PATH)
 
-buf_bin_url := https://github.com/bufbuild/buf/releases/download/v$(buf_version)/buf-$(shell uname -s)-$(shell uname -m)
+buf_arch = $(shell uname -s)-$(shell uname -m)
+# enforce x86 arch if mac m1 until tool has official support
+ifeq ($(buf_arch),Darwin-arm64)
+buf_arch = Darwin-x86_64
+endif
+buf_bin_url := https://github.com/bufbuild/buf/releases/download/v$(buf_version)/buf-$(buf_arch)
 
 $(buf): $(buf_cwd)/rules.mk
 	$(info [buf] feching version $(buf_version)...)
