f3dcf308a428409ee9423207574c151974bc107c
Oscar Söderlund <odsod@users.noreply.github.com>
2021-06-13 18:34:42 +0200

    feat: improve pattern resolution
    This change updates the API of the IAM descriptor to use a per-service
    custom struct with fields, and to unmarshal the descriptor fields from
    precomputed raw proto messages resolved during compilation.
    
    This enables resolution of resource patterns from other packages,
    without having to import the package and resolve during runtime.
    
    In practice, this fixes an issue where some resources failed to resolve
    runtime, since their packages are not imported.

--- cmd/protoc-gen-go-iam/internal/geniam/descriptor.go
+++ cmd/protoc-gen-go-iam/internal/geniam/descriptor.go
@@ -1,21 +1,34 @@
 package geniam
 
 import (
+	"fmt"
+
+	"go.einride.tech/iam/iamannotations"
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoregistry"
 )
 
 type descriptorCodeGenerator struct {
 	gen     *protogen.Plugin
-	file    *protogen.File
+	files   *protoregistry.Files
 	service *protogen.Service
+}
+
+func (c descriptorCodeGenerator) StructGoName() string {
+	return c.service.GoName + "IAMDescriptor"
 }
 
 func (c descriptorCodeGenerator) ConstructorGoName() string {
 	return "New" + c.service.GoName + "IAMDescriptor"
 }
 
-func (c descriptorCodeGenerator) GenerateCode(g *protogen.GeneratedFile) {
-	c.generateConstructor(g)
+func (c descriptorCodeGenerator) GenerateCode(g *protogen.GeneratedFile) error {
+	c.generateStruct(g)
+	if err := c.generateConstructor(g); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (c descriptorCodeGenerator) GeneratesCode() bool {
@@ -30,38 +43,130 @@ func (c descriptorCodeGenerator) GeneratesCode() bool {
 	return false
 }
 
-func (c descriptorCodeGenerator) generateConstructor(g *protogen.GeneratedFile) {
-	iamDescriptor := g.QualifiedGoIdent(protogen.GoIdent{
-		GoImportPath: "go.einride.tech/iam/iamreflect",
-		GoName:       "IAMDescriptor",
+func (c descriptorCodeGenerator) generateConstructor(g *protogen.GeneratedFile) error {
+	predefinedRoles := g.QualifiedGoIdent(protogen.GoIdent{
+		GoImportPath: "go.einride.tech/iam/proto/gen/einride/iam/v1",
+		GoName:       "PredefinedRoles",
 	})
-	newIAMDescriptor := g.QualifiedGoIdent(protogen.GoIdent{
-		GoImportPath: "go.einride.tech/iam/iamreflect",
-		GoName:       "NewIAMDescriptor",
+	longRunningOperationsAuthorizationOptions := g.QualifiedGoIdent(protogen.GoIdent{
+		GoImportPath: "go.einride.tech/iam/proto/gen/einride/iam/v1",
+		GoName:       "LongRunningOperationsAuthorizationOptions",
 	})
-	globalFiles := g.QualifiedGoIdent(protogen.GoIdent{
-		GoImportPath: "google.golang.org/protobuf/reflect/protoregistry",
-		GoName:       "GlobalFiles",
+	methodAuthorizationOptions := g.QualifiedGoIdent(protogen.GoIdent{
+		GoImportPath: "go.einride.tech/iam/proto/gen/einride/iam/v1",
+		GoName:       "MethodAuthorizationOptions",
 	})
-	serviceDescriptor := g.QualifiedGoIdent(protogen.GoIdent{
-		GoImportPath: "google.golang.org/protobuf/reflect/protoreflect",
-		GoName:       "ServiceDescriptor",
+	protoUnmarshal := g.QualifiedGoIdent(protogen.GoIdent{
+		GoImportPath: "google.golang.org/protobuf/proto",
+		GoName:       "Unmarshal",
 	})
 	fmtErrorf := g.QualifiedGoIdent(protogen.GoIdent{
 		GoImportPath: "fmt",
 		GoName:       "Errorf",
 	})
+	pReturnErr := func(format string, args ...interface{}) {
+		g.P(
+			"return nil, ", fmtErrorf, `("new `, c.service.Desc.Name(),
+			` IAM descriptor: `, fmt.Sprintf(format, args...), `: %w", err)`,
+		)
+	}
 	g.P()
 	g.P("// ", c.ConstructorGoName(), " returns a new ", c.service.GoName, " IAM descriptor.")
-	g.P("func ", c.ConstructorGoName(), "() (*", iamDescriptor, ", error) {")
-	g.P("descriptor, err := ", globalFiles, ".FindDescriptorByName(\"", c.service.Desc.FullName(), "\")")
-	g.P("if err != nil {")
-	g.P("return nil, ", fmtErrorf, `("new `, c.service.GoName, ` IAM descriptor: %w", err)`)
+	g.P("func ", c.ConstructorGoName(), "() (*", c.StructGoName(), ", error) {")
+	g.P("result := ", c.StructGoName(), "{")
+	if getPredefinedRoles(c.service) != nil {
+		g.P("PredefinedRoles: &", predefinedRoles, "{},")
+	}
+	if getLongRunningOperationsAuthorizationOptions(c.service) != nil {
+		g.P("LongRunningOperationsAuthorization: &", longRunningOperationsAuthorizationOptions, "{},")
+	}
+	for _, method := range c.service.Methods {
+		if getMethodAuthorizationOptions(method) != nil {
+			g.P(method.GoName, "Authorization: &", methodAuthorizationOptions, "{},")
+		}
+	}
 	g.P("}")
-	g.P("service, ok := descriptor.(", serviceDescriptor, ")")
-	g.P("if !ok {")
-	g.P("return nil, ", fmtErrorf, `("new `, c.service.GoName, `IAM descriptor: got non-service descriptor")`)
+	if options := getPredefinedRoles(c.service); options != nil {
+		data, err := proto.Marshal(options)
+		if err != nil {
+			return err
+		}
+		g.P("if err := ", protoUnmarshal, "(")
+		g.P(fmt.Sprintf("%#v", data), ",")
+		g.P("result.PredefinedRoles,")
+		g.P("); err != nil {")
+		pReturnErr("unmarshal pre-defined roles")
+		g.P("}")
+	}
+	if options := getLongRunningOperationsAuthorizationOptions(c.service); options != nil {
+		resolvedOptions, err := iamannotations.ResolveLongRunningOperationsAuthorizationOptions(
+			options, c.files, c.service.Desc.ParentFile().Package(),
+		)
+		if err != nil {
+			return err
+		}
+		data, err := proto.Marshal(resolvedOptions)
+		if err != nil {
+			return err
+		}
+		g.P("if err := ", protoUnmarshal, "(")
+		g.P(fmt.Sprintf("%#v", data), ",")
+		g.P("result.LongRunningOperationsAuthorization,")
+		g.P("); err != nil {")
+		pReturnErr("unmarshal long-running operations authorization")
+		g.P("}")
+	}
+	for _, method := range c.service.Methods {
+		options := getMethodAuthorizationOptions(method)
+		if options == nil {
+			continue
+		}
+		resolvedOptions, err := iamannotations.ResolveMethodAuthorizationOptions(
+			options, c.files, method.Desc.ParentFile().Package(),
+		)
+		if err != nil {
+			return err
+		}
+		data, err := proto.Marshal(resolvedOptions)
+		if err != nil {
+			return err
+		}
+		g.P("if err := ", protoUnmarshal, "(")
+		g.P(fmt.Sprintf("%#v", data), ",")
+		g.P("result.", method.GoName, "Authorization,")
+		g.P("); err != nil {")
+		pReturnErr("unmarshal %s method authorization", method.Desc.Name())
+		g.P("}")
+	}
+	g.P("return &result, nil")
 	g.P("}")
-	g.P("return ", newIAMDescriptor, "(service, ", globalFiles, ")")
+	return nil
+}
+
+func (c descriptorCodeGenerator) generateStruct(g *protogen.GeneratedFile) {
+	predefinedRoles := g.QualifiedGoIdent(protogen.GoIdent{
+		GoImportPath: "go.einride.tech/iam/proto/gen/einride/iam/v1",
+		GoName:       "PredefinedRoles",
+	})
+	longRunningOperationsAuthorizationOptions := g.QualifiedGoIdent(protogen.GoIdent{
+		GoImportPath: "go.einride.tech/iam/proto/gen/einride/iam/v1",
+		GoName:       "LongRunningOperationsAuthorizationOptions",
+	})
+	methodAuthorizationOptions := g.QualifiedGoIdent(protogen.GoIdent{
+		GoImportPath: "go.einride.tech/iam/proto/gen/einride/iam/v1",
+		GoName:       "MethodAuthorizationOptions",
+	})
+	g.P("type ", c.StructGoName(), " struct {")
+	if getPredefinedRoles(c.service) != nil {
+		g.P("PredefinedRoles *", predefinedRoles)
+	}
+	if getLongRunningOperationsAuthorizationOptions(c.service) != nil {
+		g.P("LongRunningOperationsAuthorization *", longRunningOperationsAuthorizationOptions)
+	}
+	for _, method := range c.service.Methods {
+		if getMethodAuthorizationOptions(method) != nil {
+			g.P(method.GoName, "Authorization *", methodAuthorizationOptions)
+		}
+	}
 	g.P("}")
 }
