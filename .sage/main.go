98a9752679cb480b9591b088a9b77fe9f4b15596
Oscar Söderlund <odsod@users.noreply.github.com>
2024-07-15 11:19:10 +0200

    ci: migrate to Sage-based release tooling

--- .sage/main.go
+++ .sage/main.go
@@ -9,6 +9,8 @@ import (
 	"go.einride.tech/sage/tools/sggit"
 	"go.einride.tech/sage/tools/sggo"
 	"go.einride.tech/sage/tools/sggolangcilint"
+	"go.einride.tech/sage/tools/sggoreleaser"
+	"go.einride.tech/sage/tools/sggosemanticrelease"
 	"go.einride.tech/sage/tools/sgmdformat"
 	"go.einride.tech/sage/tools/sgyamlfmt"
 )
@@ -72,4 +74,38 @@ func GoTest(ctx context.Context) error {
 func GitVerifyNoDiff(ctx context.Context) error {
 	sg.Logger(ctx).Println("verifying that git has no diff...")
 	return sggit.VerifyNoDiff(ctx)
+}
+
+func SemanticRelease(ctx context.Context, repo string, dry bool) error {
+	sg.Logger(ctx).Println("triggering release...")
+	args := []string{
+		"--allow-initial-development-versions",
+		"--allow-no-changes",
+		"--ci-condition=default",
+		"--provider=github",
+		"--provider-opt=slug=" + repo,
+	}
+	if dry {
+		args = append(args, "--dry")
+	}
+	return sggosemanticrelease.Command(ctx, args...).Run()
+}
+
+func GoReleaser(ctx context.Context, snapshot bool) error {
+	sg.Logger(ctx).Println("building Go binary releases...")
+	if err := sggit.Command(ctx, "fetch", "--force", "--tags").Run(); err != nil {
+		return err
+	}
+	args := []string{
+		"release",
+		"--clean",
+	}
+	if len(sggit.Tags(ctx)) == 0 && !snapshot {
+		sg.Logger(ctx).Printf("no git tag found for %s, forcing snapshot mode", sggit.ShortSHA(ctx))
+		snapshot = true
+	}
+	if snapshot {
+		args = append(args, "--snapshot")
+	}
+	return sggoreleaser.Command(ctx, args...).Run()
 }
